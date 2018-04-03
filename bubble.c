6e5ce0f8dcfd396b7254b5a2cbab2023717b9749
MANTIS <MANTIS>
2018-04-03 22:54:09 +0700

    modify bubble.c

--- bubble.c
+++ bubble.c
@@ -3,18 +3,15 @@
 void bubble_sort(int data[], int data_number){
     int i,j,temp;
 
-    for(i=0 ; i < data_number ; i++){//until done sorting
-
-        for(j=0; j < data_number-1 ; j++){ // for one time
+    for(i=0 ; i < data_number ; i++)//until done sorting
+        for(j=0; j < data_number-1 ; j++) // for one time
             if(data[j]  > data[j+1]){
                 //swap data
                 temp = data[j];
                 data[j] = data[j+1];
-                data[j] = temp;
+                data[j+1] = temp;
             }
-        }
-    }
-
+        
 }
 
 
@@ -23,17 +20,15 @@ int main(){
     int i,j;
 
     printf("Unsorted data : ");
-    for(i=0 ; i < 6 ; i++){
+    for(i=0 ; i < 6 ; i++)
         printf("%d ",data[i]);
-    }
     printf("\n");
 
     bubble_sort(data , 6);
 
     printf("Sorted data : ");
-    for(i=0 ; i < 6 ; i++){
+    for(i=0 ; i < 6 ; i++)
         printf("%d ",data[i]);
-    }
     printf("\n");
 
     return 0;
